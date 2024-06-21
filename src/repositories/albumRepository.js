d47c5fa00abcc0f8d0c7d4771539be5e6a46ac44
Luiz Cruz <luiz.cruz@mercadolivre.com>
2024-06-20 23:56:54 -0300

    refactor id and targetID variable

--- src/repositories/albumRepository.js
+++ src/repositories/albumRepository.js
@@ -8,17 +8,17 @@ class AlbumRepository {
     async create(description, targetId) {
         return this.repository.create(description, targetId);
     };
-    async getById(id){
-        return this.repository.getById(id);
+    async getById(albumId){
+        return this.repository.getById(albumId);
     };
     async getAll(albumId){
         return this.repository.getAll(albumId);
     };
-    async update(id, description, target_id) {
-        this.repository.update(id, description, target_id);
+    async update(albumId, description, targetId) {
+        this.repository.update(albumId, description, targetId);
     };
-    async delete (id) {
-        this.repository.delete(id);
+    async delete (albumId) {
+        this.repository.delete(albumId);
     };
 }
 
